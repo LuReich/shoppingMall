76522a6216733cd402792e4aa24c064fdeed540e
LuReich <skaehdals822@gmail.com>
2025-11-05 02:54:31 +0900

    dd

---
+++ backend/src/main/java/it/back/product/dto/ProductDeletedByAdminRequestDTO.java
@@ -0,0 +1,11 @@
+package it.back.product.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProductDeletedByAdminRequestDTO {
+    private Boolean isDeleted;
+    private String deletedByAdminReason;
+}
---
+++ backend/src/main/java/it/back/product/dto/ProductDeletedBySellerRequestDTO.java
@@ -0,0 +1,10 @@
+package it.back.product.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProductDeletedBySellerRequestDTO {
+    private String deletedBySellerReason;
+}
